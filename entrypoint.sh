ddfb2d9ed6590579231877eebc8c9eda1cc2040a
Arun <sabuarun00@gmail.com>
2026-08-13 21:04:31 +0530

    ci: enable Render deployment and access logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,7 @@ python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 3
+exec gunicorn core.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers 3 \
+    --access-logfile -

f1aa29bfee6a6b98a734363e5a3d409dfb4227cf
Arun <sabuarun00@gmail.com>
2026-08-14 15:37:51 +0530

    fix: reduce api and celery worker memory

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
-celery -A core worker --loglevel=info &
+celery -A core worker --loglevel=info --concurrency=1 &
 
 exec gunicorn core.wsgi:application \
     --bind 0.0.0.0:${PORT:-8000} \
-    --workers 3 \
+    --workers 1 \
     --access-logfile -

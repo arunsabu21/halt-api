0cdc93bf8663975a902b9521065b3d600706326b
Arun <sabuarun00@gmail.com>
2026-08-15 16:10:28 +0530

    build: add supervisord to run gunicorn and Celery reliably in one container

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,4 @@ python manage.py collectstatic --noinput
 
 celery -A core worker --loglevel=info --concurrency=1 &
 
-exec gunicorn core.wsgi:application \
-    --bind 0.0.0.0:${PORT:-8000} \
-    --workers 1 \
-    --access-logfile -
+exec supervisord -c /app/supervisord.conf
